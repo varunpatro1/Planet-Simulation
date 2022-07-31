623be374c8b2f73e5736950fb8fe22e072aa5b1b
Varun Patro <varunpatro@Varuns-MBP.lan>
2022-07-30 22:24:11 -0700

    first commit

---
+++ Planet.py
@@ -0,0 +1,94 @@
+import math
+import pygame
+
+pygame.init()
+
+W, H = 800, 800
+FONT = pygame.font.SysFont("calibri", 18)
+
+class Planet:
+    G = 6.67428e-11
+    AU = 149.6e6 * 1000
+    SCALE = 250 / AU
+    T_REF = 3600*24 # seeing one day at a time
+
+    def __init__(self,x,y,mass,radius,color, name, tang_vel):
+        self.x = x
+        self.y = y
+        self.mass = mass
+        self.radius = radius
+        self.color = color
+        self.name = name
+
+        self.xvel = 0
+        self.yvel = tang_vel
+
+        self.is_Sun = False
+        self.distance_to_sun = 0
+        self.orbit_path = []
+
+    def __str__(self):
+        return 'Name: ' + self.name + ',(x,y): (' + self.x + ',' + self.y + '), ' \
+            'radius: ' + self.radius + ', mass: ' + self.mass
+
+    def draw(self, window):
+        # draw a circle at prescribed coordinates
+        x = self.x * self.SCALE + (W/2)
+        y = self.y * self.SCALE + (H/2)
+
+        
+        if len(self.orbit_path) > 2:
+            scaled_points_for_draw = []
+            for point in self.orbit_path:
+                x,y = point
+                x = x * self.SCALE + (W/2)
+                y = y * self.SCALE + (H/2)
+                scaled_points_for_draw.append((x,y))
+
+            pygame.draw.lines(window, self.color, False, scaled_points_for_draw, 4)
+        
+        pygame.draw.circle(window, self.color, (x,y), self.radius)
+
+        if not self.is_Sun:
+                name_text = FONT.render(f"{self.name}", 1, (255, 255, 255))
+                window.blit(name_text, (x-name_text.get_width(), y + (1.25*name_text.get_height())))
+                #distance_text = FONT.render(f"{round(self.distance_to_sun/1000, 1)}km", 1, (255, 255, 255))
+                #window.blit(distance_text, (x - distance_text.get_width(), y - distance_text.get_height()))
+
+    def gravitational_attraction(self, other):
+
+        distance_x = other.x - self.x; distance_y = other.y - self.y
+        distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+
+        if other.is_Sun:
+            self.distance_to_sun = distance
+
+        force = self.G * self.mass * other.mass / distance**2
+        theta = math.atan2(distance_y, distance_x)
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
+
+        return force_x, force_y
+        
+
+    def orbit(self, planets):
+
+    
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
+            pair_fx, pair_fy = self.gravitational_attraction(planet)
+            total_fx += pair_fx
+            total_fy += pair_fy
+
+        acceleration_x = total_fx / self.mass
+        acceleration_y = total_fy / self.mass
+
+        self.xvel += acceleration_x * self.T_REF
+        self.yvel += acceleration_y * self.T_REF
+
+        self.x += self.xvel * self.T_REF
+        self.y += self.yvel * self.T_REF
+
+        self.orbit_path.append((self.x, self.y))
---
+++ main.py
@@ -0,0 +1,69 @@
+# INPSIRED BY TECH WITH TIM
+import pygame
+from Planet import *
+
+pygame.init()
+
+# Dimensions of pygame window
+W, H = 800, 800
+SOLAR_SYSTEM = pygame.display.set_mode((W,H))
+pygame.display.set_caption('PLANETARY SIMULATION')
+
+FONT = pygame.font.SysFont("calibri", 18)
+
+MASS_OF_MOON = 7.43*10e22
+
+# Colors for denoting planets
+YELLOW = (255, 255, 0)
+ORANGE = (255, 150, 125)
+BLUE = (0, 255, 210)
+RED = (255, 75, 75)
+GREY = (180, 80, 80)
+
+COLORS = [YELLOW, GREY, ORANGE, BLUE, RED]
+X_DIST_INIT = [0, -0.387, 0.723, -1, -1.524]
+Y_DIST_INIT = [0, 0, 0, 0, 0]
+MASS_INIT = [1.98892 * 10**30, 3.30 * 10**23, 4.8685 * 10**24, 5.9742 * 10**24, 6.39 * 10**23]
+RADIUS_INIT = [30, 8, 14, 16, 12]
+NAMES = ['SUN', 'MERCURY', 'VENUS', 'EARTH', 'MARS']
+# giving planets an initial velocity
+TANG_VEL_INIT = [0, 47400, 35020, 29783, 24077]
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    planet_list = []
+
+    # Initializing all planets (including sun)
+    # order will be sun, mercury, venus, earth, mars
+    for i in range(5):
+        planet = Planet(X_DIST_INIT[i] * Planet.AU, Y_DIST_INIT[i], MASS_INIT[i], RADIUS_INIT[i], COLORS[i], NAMES[i], TANG_VEL_INIT[i])
+        planet_list.append(planet)
+
+    sun = planet_list[0] #mercury = planet_list[1]; venus = planet_list[2]; earth = planet_list[3]; mars = planet_list[4]
+    sun.is_sun = True
+
+    while run:
+        clock.tick(40)
+        SOLAR_SYSTEM.fill((0, 0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        for planet in planet_list:
+            planet.orbit(planet_list)
+            planet.draw(SOLAR_SYSTEM)
+
+        pygame.display.update()
+
+    pygame.quit()
+    
+
+main()
+
+
+
+
+
